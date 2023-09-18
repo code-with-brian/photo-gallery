b13c7a60f27de317fd250ccb03b247b6c35b3f89
Brian Palmer <brian.palmer@live.com>
2023-09-18 01:57:09 -0400

    Click through images

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import cabbage from "./assets/image1.jpeg";
+import mango from "./assets/image2.jpeg";
+import fig from "./assets/image3.jpeg";
+import gaze from "./assets/image4.jpeg";
+import peach from "./assets/image5.jpeg";
+import avacado from "./assets/image6.jpeg";
 
-const App = () => (
-  <div className="App">
-    <div className="title">
-      <h1>Zesty</h1>
-      <h2>
-        A photography project <br /> by Brian Palmer
-      </h2>
-    </div>
+const images = [cabbage, mango, fig, gaze, peach, avacado];
 
-    <div className="image-container">
-      <img src="https://i.imgur.com/4V2yXZC.jpg" alt="Zesty" />
+const App = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const nextImage = () => {
+    const length = images.length - 1;
+
+    setCurrentImage((currentImage) => {
+      return currentImage < length ? currentImage + 1 : 0;
+    });
+  };
+
+  return (
+    <div className="App">
+      <div className="title">
+        <h1>Zesty</h1>
+        <h2>
+          A photography project <br /> by Brian Palmer
+        </h2>
+      </div>
+      <div className="image-container">
+        <img alt="cabbage" src={images[currentImage]} onClick={nextImage} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default App;
