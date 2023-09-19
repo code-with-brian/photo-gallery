e2ced82be74bbe6af26721830a599deb6bdac9ca
Brian Palmer <brian.palmer@live.com>
2023-09-19 05:35:17 -0400

    Refactored Code to my Semantic

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,28 @@ import fig from "./assets/image3.jpeg";
 import gaze from "./assets/image4.jpeg";
 import peach from "./assets/image5.jpeg";
 import avacado from "./assets/image6.jpeg";
+import { isPropertySignature } from "typescript";
 
 const images = [cabbage, mango, fig, gaze, peach, avacado];
 
+type LoadingProps = {
+  calculatedWidth: number;
+};
+
+const Loading = ({ calculatedWidth }: LoadingProps) => (
+  <aside>
+    <div className="loading-bar">
+      <label htmlFor="images-loaded">Loading all your favourite images...</label>
+      <progress id="images-loaded" max={100} value={calculatedWidth}/>
+    </div>
+  </aside>
+);
+
 const App = () => {
   const [currentImage, setCurrentImage] = useState(0);
+  const [numLoaded, setNumLoaded] = useState(0);
 
-  const nextImage = () => {
+  const handleClick = () => {
     const length = images.length - 1;
 
     setCurrentImage((currentImage) => {
@@ -20,18 +35,38 @@ const App = () => {
     });
   };
 
+  const handleImageLoad = () => {
+    setNumLoaded((numLoaded) => numLoaded + 1);
+  };
+
   return (
-    <div className="App">
-      <div className="title">
+    <section>
+      <header>
         <h1>Zesty</h1>
         <h2>
           A photography project <br /> by Brian Palmer
         </h2>
-      </div>
-      <div className="image-container">
-        <img alt="cabbage" src={images[currentImage]} onClick={nextImage} />
-      </div>
-    </div>
+      </header>
+      <figure>
+        {numLoaded < images.length && (
+          <Loading calculatedWidth={numLoaded / images.length * 100}/>
+        )}
+        
+        <figcaption>
+          {currentImage + 1} / {images.length}
+        </figcaption>
+        {images.map((imageURL, index) => ( 
+          <img          
+            key={imageURL} 
+            alt="" 
+            src={imageURL} 
+            onClick={handleClick} 
+            onLoad={handleImageLoad}
+            className={currentImage === index ? "display" : "hide"}
+          />
+        ))}
+      </figure>
+    </section>
   );
 };
 
